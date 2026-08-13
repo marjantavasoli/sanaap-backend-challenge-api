4548662b51e239e57011a9427b354467c66c6ba2
marjan <marjantavasoli73@gmail.com>
2026-08-13 23:03:25 +0330

    fix: fix a small bug with webhook

--- config/asgi.py
+++ config/asgi.py
@@ -8,15 +8,12 @@ from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from common.websocket_authentication import JWTWebsocketAuthenticationMiddleware
 from documents.ws_routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            JWTWebsocketAuthenticationMiddleware(URLRouter(websocket_urlpatterns))
-        ),
+        "websocket": JWTWebsocketAuthenticationMiddleware(URLRouter(websocket_urlpatterns)),
     }
 )
